d41e68c6261b41657f9ce51023ac26ed9b527e57
Jisang Kim <jjoo0815@gmail.com>
2024-07-16 11:04:27 +0900

    babylon scene init

---
+++ src/components/BabylonScene.vue
@@ -0,0 +1,27 @@
+<template>
+  <canvas ref="bjsCanvas" width="500" height="500" />
+
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+import { createScene } from "../scenes/MainScene.js";
+
+
+export default {
+  name: "BabylonScene",
+  setup() {
+    const bjsCanvas = ref(null);
+
+    onMounted(() => {
+      if (bjsCanvas.value) {
+        createScene(bjsCanvas.value);
+      }
+    });
+
+    return {
+      bjsCanvas,
+    };
+  },
+};
+</script>
